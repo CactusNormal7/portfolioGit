4f3d43d915615cb1e23bb2fe880b0d4adb456c8d
CactusNormal7 <jules.besson7@gmail.com>
2023-05-16 15:31:27 +0200

    d

--- portfolio/src/routes/+page.svelte
+++ portfolio/src/routes/+page.svelte
@@ -1,40 +1,105 @@
 <script lang="ts">
-    let circle: HTMLDivElement
+    import { each, onMount } from "svelte/internal";
+    import type { PageData } from "./$types";
     
+    let circle: HTMLDivElement;
+    let buttonviewmore: HTMLButtonElement;
+    let projectlistdiv: HTMLDivElement;
+    let allcard: NodeListOf<HTMLElement>;
+
+    export let data: PageData;
+    $: ({ projects } = data);
 
     function mouseHandler(e: any) {
-        circle.style.left = `${e.clientX - 50}px`
-        circle.style.top = `${e.clientY - 40}px`
+        circle.style.left = `${e.clientX - 50}px`;
+        circle.style.top = `${e.clientY - 40}px`;
     }
-    
 
+    onMount(() => {
+        allcard = projectlistdiv.querySelectorAll(".cardivall")
+    });
+
+    const viewMoreOnClick = (idclicked: number, idclick: string) => {
+        if(allcard[idclicked].querySelector<HTMLButtonElement>(`#${idclick}`)?.getAttribute("data-test") === "false") {
+            allcard[idclicked].querySelector<HTMLDivElement>(".cardDivTop")!.style.height = "300px";
+            allcard[idclicked].querySelector<HTMLButtonElement>(`#${idclick}`)?.setAttribute("data-test", "true");
+            let divDesc = document.createElement("div")
+            divDesc.innerText = projects[idclicked].desc
+            divDesc.style.color = "white";
+            divDesc.style.fontSize = "14px";
+            divDesc.style.fontFamily = "'Poppins', sans-serif";
+            divDesc.style.fontWeight = "bolder"
+            divDesc.style.marginTop = "60px"
+            divDesc.style.maxWidth = "80%"
+            divDesc.classList.add("descriptionTxt")
+            
+            allcard[idclicked].querySelector<HTMLDivElement>(".cardDivTop")?.appendChild(divDesc);
+        }else {
+            allcard[idclicked].querySelector<HTMLDivElement>(".cardDivTop")!.style.height = "150px";
+            allcard[idclicked].querySelector<HTMLButtonElement>(`#${idclick}`)?.setAttribute("data-test", "false");
+            let ratio = allcard[idclicked].querySelector<HTMLDivElement>(".descriptionTxt")
+            ratio?.remove();
+            
+        }
+    }
+
+    
 </script>
 
-<main on:mousemove={mouseHandler}>
-    <div class="containerAll" >
+<main id="main" on:mousemove={mouseHandler}>
+    <div style="color: white;" />
+    <div class="containerAll">
+        <div bind:this={circle} id="circle" />  
         <div id="mainTittleContainer">
-            <h1 id="mainTittle">Jules Besson, developeur web.</h1>
+            <h1 id="mainTittle">Hi, I'm Jules Besson, <br> Welcome to my portfolio</h1>
         </div>
-        <div bind:this={circle} id="circle" /> 
+    </div>
+    <div style="display: flex; justify-content: center">
+        <div class="projectlistdiv" bind:this={projectlistdiv}>
+            {#each projects as items, i}
+            <div class="cardivall" id="cardivall{i}">
+                <div class="cardDivTop">
+                    <h2 style="color: white;margin: 0; margin-left: 10px; font-family: 'Poppins', sans-serif;margin-top: 50px;">{items.name}</h2>
+                </div>
+                <div  class="cardDivBot">
+                    <button on:click={() => viewMoreOnClick(i, `buttonviewmore${i}`)} class="buttonviewmore" id="buttonviewmore{i}" data-test="false">view more</button>
+                </div>
+            </div>
+            {/each}
+        </div>
     </div>
 </main>
-
+    
 <style>
     @import url("https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500&display=swap");
 
     :global(body) {
         background: #0f0f0f;
-        cursor: url('/static/favicon.png'), auto;
-        overflow: hidden;
+        /* cursor: url(''), auto; */
+        overflow-x: hidden;
     }
 
     .containerAll {
+        padding-top: 10vh;
         display: flex;
         justify-content: center;
         width: 100vw;
-        margin-top: 10vh;
-        height: 100vh;
     }
+
+    .buttonviewmore {
+        background-color: transparent;
+        border: 3px solid;
+        font-size: 20px;
+        font-family: "Poppins", sans-serif;
+        transition: 0.5s ease;
+        font-weight: bolder;
+    }
+
+    .buttonviewmore:hover {
+        color:white;
+        background-color: black;
+    }
+
     #circle {
         position: absolute;
         width: 100px;
@@ -47,9 +112,56 @@
         pointer-events: none;
     }
 
+    #main {
+        height: 100vh;
+    }
+
     #mainTittle {
         font-size: 60px;
         color: white;
         font-family: "Poppins", sans-serif;
+        padding-right: 200px;
+    }
+
+    #ProjectTittle {
+        margin-left: 30px;
+        color: white;
+        font-family: "Poppins", sans-serif;
+    }
+
+    .cardDivBot {
+        width: 300px;
+        height: 100px;
+        margin-left: 20px;
+        margin-right: 20px;
+        background-color: white;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+
+    .cardDivTop {
+        width: 292px;
+        height: 150px;
+        margin-left:20px;
+        transition: 0.6s ease;
+        margin-right: 20px;
+        margin-top: 30px;
+        flex-direction: column;
+        align-items: center;
+        border: 4px solid;
+        display: flex;
+        border-color: white;
+    }
+
+    .containerAll {
+        display: flex;
+    }
+
+    .projectlistdiv{
+        justify-content: center;
+        display: flex;
+        max-width: 70vw;
+        flex-wrap: wrap;
     }
 </style>
---
+++ portfolio/src/routes/test/+page.svelte
@@ -0,0 +1,48 @@
+<script lang="ts">
+    import { onMount } from 'svelte';
+    import { each } from "svelte/internal";
+   import type { PageData } from "../$types";
+    export let data : PageData
+
+    $: ({projects} = data)
+    let currentImageIndex = 0;
+  
+    // onMount(() => {
+    //   const interval = setInterval(() => {
+    //     currentImageIndex = (currentImageIndex + 1) % projects.length;
+    //   }, 3000);
+  
+    //   return () => {
+    //     clearInterval(interval);
+    //   };
+    // });
+    
+  </script>
+  
+  <style>
+    .carousel {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      height: 300px;
+      overflow: hidden;
+    }
+  
+    .carousel img {
+      max-width: 100%;
+      max-height: 100%;
+      object-fit: cover;
+      transition: transform 0.5s;
+    }
+  
+    .carousel img:not(:first-child) {
+      transform: translateX(-100%);
+    }
+  </style>
+  
+  <div class="carousel">
+    {#each projects as item, i}
+    <div style="transform: translateX({(i- currentImageIndex) *100}%)">{item.name}</div>
+      <!-- <img src={image} alt="Image {i + 1}" style="transform: translateX({(i - currentImageIndex) * 100}%)" /> -->
+    {/each}
+  </div>
